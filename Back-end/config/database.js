e5c90c7f80f76a7726b7d601c0858f3fa6bafd36
dumbsito <lean.04river@gmail.com>
2023-04-05 14:08:15 -0300

    register and login

--- Back-end/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: process.env.MONGO_URL || "mongodb://127.0.0.1/express-server",
-    secret: "mysecret"
-}
--- Back-end/index.js
+++ Back-end/index.js
@@ -1,57 +1,94 @@
-const express = require ("express");
-const path = require ("path");
-const bodyParser = require ("body-parser");
-const cors = require ("cors");
-const passport = require ("passport");
-const mongoose = require ("mongoose");
-const config = require("./config/database");
-const session = require("express-session");
-
-mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set("strictQuery", true);
-
-mongoose.connection.on('connected', ()=>{
-    console.log("connected to database " + config.database);
-});
-
-mongoose.connection.on('error', (err)=>{
-    console.log("Database error " + err);
-});
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-app.use(session({ secret: 'melody hensley is my spirit animal' }));
 
-const users = require("./routes/users");
-
-const port = 3000;
-
-
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //allow CORS
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization'); //tell which extra headers the app might have
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    console.log('Middleware');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*"); //allow CORS
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept, Authorization"
+  ); //tell which extra headers the app might have
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  console.log("Middleware");
+  next();
 });
 
+let corsOptions = {
+  origin: ["http://localhost:49875/"],
+  credentials: true,
+};
 
-  let corsOptions = {
-    origin: ["https://notesapp-78b.netlify.app/"],
-    credentials: true
+app.use(cors(corsOptions));
+
+const port = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
-  
-  app.use(cors(corsOptions));
-require("./config/passport")(passport);
+});
 
-app.use("/users", users);
-app.get("/",(req,res)=>{
-    res.send("hello from home")
-})
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email: email });
 
-app.listen(port, ()=>{
-    console.log("The server started on port " + port);
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token: token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
